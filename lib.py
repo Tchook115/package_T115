8d010b57c1fbfd1c6b050539d470a372797fce16
Alexandre Bernard <alexandre.bernard115@gmail.com>
2021-08-23 12:29:38 +0200

    Initial Push

---
+++ lib.py
@@ -0,0 +1,10 @@
+def try_me():
+    a = input("Who's the best? ")
+    if a == 'Alex' or a == 'Alexandre' or a == 'Tchook':
+        print("Bravo, you're right")
+    if a == 'Etienne':
+        print('Are you kidding me?')
+    else:
+        print('Haha looser')
+
+try_me()
